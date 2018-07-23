ac3d66e23f76c9465409ed4efa107ef1cf8e38b0
Noelle Foster <uufarmgirl@gmail.com>
2018-07-23 16:02:28 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function iterativeLog(array){
+  array.forEach(console.log(`${index}: ${element}`))
+}
+
+function iterate(callback){
+  arr=new Array()
+  arr=[1,2,3,4,5,6]
+  iterativeLog(arr)
+  return arr
+
+}
